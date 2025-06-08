c4269fd8b9a7187b14a621c472bfee848c41b4cb
Bud-Macaulay <budmacaulay97@outlook.com>
2025-06-08 15:16:35 +0200

    Update header.js
    Updated .mc-header to (try) to always be ontop

--- header.js
+++ header.js
@@ -11,7 +11,13 @@ body {
   background: #fff;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.04);
   margin: 0;
-}
+  position: fixed !important;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+  }
+  
 .mc-header-separator {
   width: 100%;
   height: 16px;
