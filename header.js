8bffbec3cb696c202c9962df875f3fb661ac35f5
Edan Bainglass <45081142+edan-bainglass@users.noreply.github.com>
2026-07-16 12:28:02 +0200

    Move the background color rule to the header class
    Applying the background color only to the header separators leaves
    the body background color exposed on the margins of the breadcrumbs.
    Here we move the CSS rule to the `mc-header` CSS selector, covering
    the full component.

--- header.js
+++ header.js
@@ -7,12 +7,12 @@ const mc_header_css = `
 body {
   margin: 0;
 }
-  
+.mc-header {
+  background-color: aliceblue;
+}
 .mc-header-separator {
   width: 100%;
   height: 16px;
-  background-color: aliceblue;
-
 }
 .mc-header .mc-nav {
   background-color: #fff;
